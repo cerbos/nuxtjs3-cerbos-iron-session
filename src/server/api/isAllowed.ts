571d6de72ee739d3b3c3a76d07ec434e88ec9602
Allan Jeremy <aj.dev254@gmail.com>
2023-02-01 15:10:36 +0300

    fix: fixed isAllowed API

--- src/server/api/isAllowed.ts
+++ src/server/api/isAllowed.ts
@@ -4,7 +4,6 @@ const cerbos = new Cerbos("localhost:3593", { tls: false });
 
 export default defineEventHandler(async (event) => {
   const data = await readBody(event);
-  console.log("payload: ", data);
 
   const isAllowed = await cerbos.isAllowed(data);
 
